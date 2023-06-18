9cd75a9d8b4b2d657507517b8d72c0aa2c70e652
gh-337 <neyro337@gmail.com>
2023-06-19 02:13:00 +0300

    in

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.header__burger').click(function(event){
         $('.header__burger,.dmenu').toggleClass('active');
+        $('body').toggleClass('lock');
     });
 }); //click bur open / close
